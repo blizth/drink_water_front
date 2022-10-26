88f0878381335cc37d36769f77f0d55eb6eea11d
Blizth <xray496@gmail.com>
2022-10-26 23:38:57 +0200

    Added Chakra Ui and Rive Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
@@ -7,9 +8,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
